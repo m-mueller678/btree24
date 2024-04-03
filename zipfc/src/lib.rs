d1e8d6b5db9b6472e5496ccffb98db7ffab66b40
m-mueller678 <marcus.mueller.678@gmail.com>
2024-04-03 14:11:18 +0200

    move random generation to rust

--- zipfc/src/lib.rs
+++ zipfc/src/lib.rs
@@ -1,94 +1,245 @@
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::slice::from_raw_parts_mut;
+#![allow(clippy::missing_safety_doc)]
 use rand::distributions::Uniform;
 use rand::prelude::*;
-use zipf::ZipfDistribution;
-use bloomfilter::Bloom;
+use rand::Fill;
 use rand_xoshiro::Xoshiro256StarStar;
+use rayon::prelude::*;
+use rip_shuffle::RipShuffleParallel;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashSet;
+use std::ffi::{c_char, CStr};
+use std::fmt::{Debug, Formatter};
+use std::fs::File;
+use std::hash::{Hash, Hasher};
+use std::io::{BufRead, BufReader};
+use std::mem::{size_of, MaybeUninit};
+use std::slice::from_raw_parts;
+use zipf::ZipfDistribution;
 
-fn make_rand(seed:u64,purpose:&str)->Xoshiro256StarStar{
-    let long_seed = (0..4).flat_map(|i|{
-        let mut h=DefaultHasher::new();
-        i.hash(&mut h);
-        seed.hash(&mut h);
-        purpose.hash(&mut h);
-        h.finish().to_le_bytes()
-    }).collect::<Vec<u8>>();
-    Xoshiro256StarStar::from_seed(long_seed.try_into().unwrap())
+unsafe impl Send for Key {}
+
+unsafe impl Sync for Key {}
+
+#[repr(C)]
+pub struct Key {
+    data: *const u8,
+    len: u64,
+}
+
+impl Debug for Key {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut t = f.debug_tuple("Key");
+        let bytes = unsafe { from_raw_parts(self.data, self.len as usize) };
+        t.field(&String::from_utf8_lossy(bytes));
+        t.field(&bytes);
+        t.finish()
+    }
+}
+
+impl Key {
+    fn from_vec(x: Vec<u8>) -> Self {
+        let x = x.leak();
+        Key {
+            data: x.as_ptr(),
+            len: x.len() as u64,
+        }
+    }
+}
+
+pub type MainRng = Xoshiro256StarStar;
+
+pub unsafe extern "C" fn zipfc_load_keys(
+    rng: *mut MainRng,
+    name: *const c_char,
+    count: u32,
+    int_density: f64,
+    partition_count: u32,
+) -> *const Key {
+    assert!(int_density <= 1.0);
+    assert!(int_density >= 0.05);
+    let name = CStr::from_ptr(name);
+    let rng = &mut *rng;
+    let keys = match name.to_str().unwrap() {
+        "rng4" => generate_parallel::<u32>(count, rng),
+        "rng8" => generate_parallel::<u32>(count, rng),
+        "int" => {
+            let gen_count = ((count as f64) / int_density).round();
+            assert!(gen_count < 1.0e9);
+            let mut generated: Vec<u32> = (0..gen_count as u32).into_par_iter().collect();
+            generated.par_shuffle(rng);
+            generated.truncate(count as usize);
+            generated
+                .into_par_iter()
+                .map(|x| Key::from_vec(x.to_be_bytes().to_vec()))
+                .collect()
+        }
+        "partitioned_id" => {
+            let mut next_id = vec![0u32; partition_count as usize];
+            let dist = Uniform::new(0, partition_count);
+            (0..count)
+                .map(|_| {
+                    let partition = dist.sample(rng);
+                    let id = next_id[partition as usize];
+                    next_id[partition as usize] += 1;
+                    Key::from_vec(
+                        [partition.to_be_bytes(), id.to_be_bytes()]
+                            .into_iter()
+                            .flatten()
+                            .collect(),
+                    )
+                })
+                .collect()
+        }
+        x => {
+            let path = x.strip_prefix("file:").expect("bad key set name");
+            let mut lines: Vec<Key> = BufReader::new(File::open(path).unwrap())
+                .lines()
+                .map(|l| Key::from_vec(l.unwrap().into_bytes()))
+                .collect();
+            lines.par_shuffle(rng);
+            lines
+        }
+    };
+    dbg!(&keys);
+    assert_eq!(keys.len(), count as usize);
+    keys.leak().as_ptr()
+}
+
+fn generate_parallel<T>(count: u32, rng: &mut MainRng) -> Vec<Key>
+where
+    [T]: Fill,
+    T: From<u8> + Clone + Send + Sync + Ord + Hash,
+    rand::distributions::Standard: rand::distributions::Distribution<T>,
+{
+    let mut out = vec![T::from(0); count as usize];
+    let chunks: Vec<(&mut [T], MainRng)> = out
+        .chunks_mut(count as usize / 32)
+        .zip(rng_stream(rng))
+        .collect();
+    chunks.into_par_iter().for_each(|(chunk, mut rng)| {
+        rng.fill(chunk);
+    });
+    out.par_sort();
+    out.dedup();
+    let mut new_candidates = HashSet::new();
+    while out.len() + new_candidates.len() < count as usize {
+        let new_candidate = rng.gen();
+        if out.binary_search(&new_candidate).is_err() {
+            new_candidates.insert(new_candidate);
+        }
+    }
+    out.extend(new_candidates);
+    out.par_shuffle(rng);
+    println!("generating {} more keys", count as usize - out.len());
+    out.into_par_iter()
+        .map(|x| Key {
+            data: Box::into_raw(Box::<T>::new(x)) as *const u8,
+            len: size_of::<T>() as u64,
+        })
+        .collect()
+}
+
+fn rng_stream(rng: &mut MainRng) -> impl Iterator<Item = MainRng> + '_ {
+    std::iter::repeat_with(|| {
+        let x = rng.clone();
+        rng.jump();
+        x
+    })
+}
+
+pub unsafe extern "C" fn create_zipfc_rng(
+    seed: u64,
+    thread: u64,
+    purpose: *const c_char,
+) -> *mut MainRng {
+    let purpose = CStr::from_ptr(purpose);
+    let long_seed = (0..4)
+        .flat_map(|i| {
+            let mut h = DefaultHasher::new();
+            i.hash(&mut h);
+            seed.hash(&mut h);
+            thread.hash(&mut h);
+            purpose.hash(&mut h);
+            h.finish().to_le_bytes()
+        })
+        .collect::<Vec<u8>>();
+    Box::into_raw(Box::new(MainRng::from_seed(long_seed.try_into().unwrap())))
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn generate_zipf(seed: u64, key_count: u32, zipf_parameter: f64, data_out: *mut u32, count: u32) {
-    let mut rng = make_rand(seed,"zipf");
+pub unsafe extern "C" fn generate_workload_c(
+    rng: *mut MainRng,
+    key_count: u32,
+    zipf_parameter: f64,
+    count: u32,
+) -> *const u32 {
+    let rng = &mut *rng;
+    let mut out = Vec::with_capacity(count as usize);
+    fill_zipf(
+        &mut *rng,
+        &mut out.spare_capacity_mut()[..count as usize],
+        key_count,
+        zipf_parameter,
+    );
+    out.set_len(count as usize);
+    dbg!(&out);
+    out.leak().as_ptr()
+}
+
+fn fill_zipf(rng: &mut Xoshiro256StarStar, dst: &mut [MaybeUninit<u32>], key_count: u32, p: f64) {
     const SHUFFLE_SCALE: usize = 1 << 20;
     if key_count == 0 {
-        assert_eq!(count, 0);
+        assert!(dst.is_empty());
         return;
     }
-    let generator = if zipf_parameter > 0.0 {
-        Ok(ZipfDistribution::new(key_count as usize *  SHUFFLE_SCALE , zipf_parameter).unwrap())
+    let generator = if p > 0.0 {
+        Ok(ZipfDistribution::new(key_count as usize * SHUFFLE_SCALE, p).unwrap())
     } else {
         Err(Uniform::new(0, key_count))
     };
-    for i in 0..count as usize {
-        data_out.add(i).write(match generator {
-            Ok(zipf) => {
-                // this modulo zipf sampling is described in the ycsb paper
-                let mut hasher = DefaultHasher::default();
-                hasher.write_usize(zipf.sample(&mut rng));
-                let x = (hasher.finish() % key_count as u64) as u32;
-                x
-            }
-            Err(uniform) => uniform.sample(&mut rng),
-        });
-    }
+
+    let chunks: Vec<_> = dst.chunks_mut(1 << 16).zip(rng_stream(rng)).collect();
+    chunks.into_par_iter().for_each(|(chunk, mut rng)| {
+        for d in chunk {
+            let x = match &generator {
+                Ok(zipf) => {
+                    // this modulo zipf sampling is described in the ycsb paper
+                    let mut hasher = DefaultHasher::default();
+                    hasher.write_usize(zipf.sample(&mut rng));
+                    (hasher.finish() % key_count as u64) as u32
+                }
+                Err(uniform) => uniform.sample(&mut rng),
+            };
+            d.write(x);
+        }
+    });
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn generate_workload_e(seed: u64, zipf_parameter: f64, base_key_count: u32, available_key_count: u32, op_count: u32, ops_out: *mut u32) {
+pub unsafe extern "C" fn generate_workload_e(
+    rng: *mut MainRng,
+    zipf_parameter: f64,
+    base_key_count: u32,
+    available_key_count: u32,
+    op_count: u32,
+) -> *const u32 {
     let insertions = op_count / 20;
     let post_insert_key_count = base_key_count + insertions;
     assert!(post_insert_key_count <= available_key_count);
-    generate_zipf(seed, post_insert_key_count,zipf_parameter,ops_out.add(insertions as usize),op_count-insertions);
-    for i in 0..insertions {
-        ops_out.add(i as usize).write((base_key_count + i) | (1 << 31));
+    let mut out = Vec::with_capacity(op_count as usize);
+    let to_fill = &mut out.spare_capacity_mut()[..op_count as usize];
+    let rng = &mut *rng;
+    for (i, dst) in to_fill[..insertions as usize].iter_mut().enumerate() {
+        dst.write((base_key_count + i as u32) | (1 << 31));
     }
-    let out = from_raw_parts_mut(ops_out,op_count as usize);
-    out.shuffle(&mut make_rand(seed,"work-e"));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn generate_rng4(seed: u64, num_elements: u32, out: *mut u32) {
-    let mut rng = make_rand(seed,"rng4");
-    let num_elements = num_elements as usize;
-    let mut bloom = Bloom::new_for_fp_rate_with_seed(num_elements, 0.01, &rng.gen());
-    for i in 0..num_elements {
-        let next = loop {
-            let candidate: u32 = rng.gen();
-            if !bloom.check(&candidate) {
-                break candidate;
-            }
-        };
-        bloom.set(&next);
-        out.add(i).write(next);
-    }
-}
-
-
-#[no_mangle]
-pub unsafe extern "C" fn generate_rng8(seed: u64, num_elements: u32, out: *mut u64) {
-    let mut rng = make_rand(seed,"rng8");
-    let num_elements = num_elements as usize;
-    let mut bloom = Bloom::new_for_fp_rate_with_seed(num_elements, 0.01, &rng.gen());
-    for i in 0..num_elements {
-        let next = loop {
-            let candidate: u64 = rng.gen();
-            if !bloom.check(&candidate) {
-                break candidate;
-            }
-        };
-        bloom.set(&next);
-        out.add(i).write(next);
-    }
+    fill_zipf(
+        &mut *rng,
+        &mut to_fill[insertions as usize..],
+        available_key_count,
+        zipf_parameter,
+    );
+    out.set_len(op_count as usize);
+    out.par_shuffle(rng);
+    dbg!(&out);
+    out.leak().as_ptr()
 }
--- zipfc/zipfc.h
+++ zipfc/zipfc.h
@@ -5,12 +5,31 @@
 
 extern "C" {
 
-void
-generate_zipf(uint64_t seed, uint32_t key_count, double zipf_parameter, uint32_t *index_out, uint32_t sample_count);
-void generate_workload_e(uint64_t seed, double zipf_parameter, uint32_t base_key_count, uint32_t available_key_count,
-                         uint32_t op_count, uint32_t *ops_out);
-void generate_rng4(uint64_t seed, uint32_t count, uint32_t *out);
-void generate_rng8(uint64_t seed, uint32_t count, uint64_t *out);
+struct ZipfcRng;
+
+struct Key {
+    uint8_t *data;
+    uint64_t len;
+};
+
+Key *zipfc_load_keys(ZipfcRng *rng, const char *name, uint32_t count, double density, uint32_t partition_count);
+
+ZipfcRng *create_zipfc_rng(uint64_t u64, uint64_t thread, const char *purpose);
+
+uint32_t *generate_workload_c(
+        ZipfcRng *rng,
+        uint32_t key_count,
+        double zipf_parameter,
+        uint32_t sample_count
+);
+uint32_t *generate_workload_e(ZipfcRng *rng,
+                              double zipf_parameter,
+                              uint32_t base_key_count,
+                              uint32_t available_key_count,
+                              uint32_t op_count,
+);
+void generate_rng4(ZipfcRng *rng, uint32_t count, uint32_t *out);
+void generate_rng8(ZipfcRng *rng, uint32_t count, uint64_t *out);
 }
 
 #endif //BTREE24_ZIPFC_H
