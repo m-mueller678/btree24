b6df917d8e3f6c3d113d3e12e9509fae78752299
m-mueller678 <marcus.mueller.678@gmail.com>
2024-05-29 14:40:20 +0200

    implement TlxWrapper, test fails

--- btree/TlxWrapper.cpp
+++ btree/TlxWrapper.cpp
@@ -1,2 +1,124 @@
+#include "config.hpp"
 #ifdef USE_STRUCTURE_TLX
+
+#include "TlxWrapper.hpp"
+#include "common.hpp"
+
+
+uint32_t unwrap_int_key(std::span<uint8_t> key) {
+    if (key.size() != 4)
+        throw;
+    return __builtin_bswap32(loadUnaligned<uint32_t>(key.data()));
+}
+
+TlxWrapper::TlxWrapper(bool isInt) : isInt(false), integers(), strings() {}
+
+TlxWrapper::~TlxWrapper() {}
+
+void TlxWrapper::lookupImpl(std::span<uint8_t> key, std::function<void(std::span<uint8_t>)> callback) {
+    if (isInt) {
+        auto it = integers.find(unwrap_int_key(key));
+        if (it != integers.end()) {
+            callback(it->second);
+        }
+    } else {
+        auto it = strings.find(KeyType{key});
+        if (it != strings.end()) {
+            callback(it->second);
+        }
+    }
+}
+
+void TlxWrapper::insertImpl(std::span<uint8_t> key, std::span<uint8_t> payload) {
+    std::vector<uint8_t> p{payload.begin(), payload.end()};
+    if (isInt) {
+        integers.insert(std::make_pair(unwrap_int_key(key), p));
+    } else {
+        KeyType k{key};
+        k.makeOwned();
+        strings.insert(std::make_pair(std::move(k), p));
+    }
+}
+
+KeyType::KeyType(const KeyType &other) : data(other.data), owned(other.owned) {
+    if (owned) {
+        owned = false;
+        makeOwned();
+    }
+}
+
+void KeyType::makeOwned() {
+    if (owned)
+        abort();
+    auto ptr = new uint8_t[data.size()];
+    memcpy(ptr, data.data(), data.size());
+    data = {ptr, data.size()};
+}
+
+bool KeyType::operator<(const KeyType &other) const {
+    return std::lexicographical_compare(data.begin(), data.end(), other.data.begin(), other.data.end());
+}
+
+bool KeyType::operator<=(const KeyType &other) const {
+    return !(other < *this);
+}
+
+KeyType::~KeyType() {
+    if (owned)
+        delete[] data.data();
+}
+
+KeyType::KeyType(KeyType &&other) : data(other.data), owned(other.owned) {
+    other.owned = false;
+}
+
+KeyType &KeyType::operator=(const KeyType &other) {
+    if (owned)
+        delete[] data.data();
+    data = other.data;
+    owned = false;
+    if (other.owned)
+        makeOwned();
+    return *this;
+}
+
+KeyType &KeyType::operator=(KeyType &&other) {
+    if (owned)
+        delete[] data.data();
+    data = other.data;
+    owned = other.owned;
+    other.owned = false;
+    return *this;
+}
+
+static uint8_t EMPTY_KEY = 0;
+
+KeyType::KeyType() : data({&EMPTY_KEY, 0}), owned(false) {}
+
+void TlxWrapper::range_lookupImpl(std::span<uint8_t> key, uint8_t *keyOutBuffer,
+                                  const std::function<bool(unsigned int, std::span<uint8_t>)> &found_record_cb) {
+    if (isInt) {
+        auto it = integers.find(unwrap_int_key(key));
+        while (it != integers.end()) {
+            union {
+                uint32_t x;
+                uint8_t b[4];
+            };
+            x = __builtin_bswap32(it->first);
+            memcpy(keyOutBuffer, b, 4);
+            if (!found_record_cb(4, it->second))
+                return;
+            ++it;
+        }
+    } else {
+        auto it = strings.find(KeyType{key});
+        while (it != strings.end()) {
+            memcpy(keyOutBuffer, it->first.data.data(), it->first.data.size());
+            if (!found_record_cb(it->first.data.size(), it->second))
+                return;
+            ++it;
+        }
+    }
+}
+
 #endif
--- btree/TlxWrapper.hpp
+++ btree/TlxWrapper.hpp
@@ -4,22 +4,48 @@
 
 #ifdef USE_STRUCTURE_TLX
 
+#include <algorithm>
 #include <cstdint>
 #include <functional>
 #include "vmache.hpp"
 #include "nodes.hpp"
 #include "container/btree_map.hpp"
 
+struct KeyType {
+    std::span<uint8_t> data;
+    bool owned;
+
+    KeyType();
+
+    KeyType(std::span<uint8_t> data) : data(data), owned(false) {}
+
+    KeyType(const KeyType &other);
+
+    KeyType(KeyType &&other);
+
+    ~KeyType();
+
+    void makeOwned();
+
+    bool operator<(const KeyType &other) const;
+
+    bool operator<=(const KeyType &other) const;
+
+    KeyType &operator=(const KeyType &other);
+
+    KeyType &operator=(KeyType &&other);
+};
+
 struct TlxWrapper {
     TlxWrapper(bool isInt);
 
     ~TlxWrapper();
 
-    tlx::btree_map<uint32_t, std::vector<uint8_t>, std::less<uint32_t>, tlx::btree_default_traits<uint32_t, uint32_t, BTREE_CMAKE_PAGE_SIZE, BTREE_CMAKE_PAGE_SIZE>,
-            std::allocator<uint32_t>, true> integers;
-
-    tlx::btree_map<std::vector<uint8_t>, std::vector<uint8_t>, std::less<std::vector<uint8_t>>, tlx::btree_default_traits<std::vector<uint8_t>, std::vector<uint8_t>, BTREE_CMAKE_PAGE_SIZE, BTREE_CMAKE_PAGE_SIZE>,
-            std::allocator<std::vector<uint8_t>>, true> strings;
+    bool isInt;
+    tlx::btree_map<uint32_t, std::vector<uint8_t>, std::less<uint32_t>, tlx::btree_default_traits<uint32_t, std::vector<uint8_t>, BTREE_CMAKE_PAGE_SIZE, BTREE_CMAKE_PAGE_SIZE>,
+            std::allocator<std::pair<uint32_t, std::vector<uint8_t>>>, true> integers;
+    tlx::btree_map<KeyType, std::vector<uint8_t>, std::less<KeyType>, tlx::btree_default_traits<KeyType, std::vector<uint8_t>, BTREE_CMAKE_PAGE_SIZE, BTREE_CMAKE_PAGE_SIZE>,
+            std::allocator<std::pair<KeyType, std::vector<uint8_t>>>, true> strings;
 
     void lookupImpl(std::span<uint8_t> key, std::function<void(std::span<uint8_t>)> callback);
 
@@ -27,12 +53,6 @@ struct TlxWrapper {
 
     void range_lookupImpl(std::span<uint8_t> key, uint8_t *keyOutBuffer,
                           const std::function<bool(unsigned int, std::span<uint8_t>)> &found_record_cb);
-
-    void trySplit(GuardX<AnyNode> node, GuardX<AnyNode> parent, std::span<uint8_t> key);
-
-    void ensureSpace(PID innerNode, std::span<uint8_t> key);
-
-    void nodeCount(std::array<uint32_t, TAG_END + 1> &counts);
 };
 
 #endif
