8ff41e3e97d489d8b478350b5a5d373527d7e6e4
m-mueller678 <marcus.mueller.678@gmail.com>
2024-06-07 13:07:16 +0200

    add counter macro

--- btree/common.hpp
+++ btree/common.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <cassert>
 #include <cstring>
+#include <mutex>
+#include <atomic>
 
 #define ASSUME(x) do{assert(x); if (!(x)) __builtin_unreachable();}while(0)
 
@@ -56,4 +58,18 @@ unsigned commonPrefixLength(std::span<uint8_t> a, std::span<uint8_t> b);
 
 std::span<uint8_t> optimistic_memcpy(uint8_t *buffer, uint32_t offset, std::span<uint8_t>);
 
+static std::mutex print_mutex;
+
+#define COUNTER(label, increment, interval) \
+do{                                         \
+    static std::atomic<uint64_t > label ## _count =0; \
+    static std::atomic<uint64_t > label ## _sum =0;   \
+    uint64_t count = 1+(label ## _count).fetch_add(1,std::memory_order_relaxed); \
+    uint64_t sum = increment+(label ## _sum).fetch_add(increment,std::memory_order_relaxed); \
+    if(count % ((uint64_t)(1)<<interval) == 0 ){\
+        print_mutex.lock();                   \
+        std::cout<<"counter "<<#label<<" "<<sum<<"/"<<count<<"="<<double(sum)/count<<std::endl; \
+        print_mutex.unlock();\
+    }\
+}while(0);
 #endif //BTREE24_COMMON_HPP
